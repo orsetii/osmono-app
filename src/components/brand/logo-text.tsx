0e36995d13326b9f7c12cc8d74c9fedea8bd8e3a
Tom French <hello@orseti.com>
2024-01-05 01:39:55 +0000

    `LogoText` function

---
+++ src/components/brand/logo-text.tsx
@@ -0,0 +1,11 @@
+import { siteConfig } from "@/config/site";
+
+export function LogoText() {
+    return (
+        <>
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
+        </>
+    )
+}
